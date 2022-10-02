9ba845d2f569211e8d4025cc6c49125589e90cca
number44 <lesiewicz.daniel.dev@gmail.com>
2022-10-02 19:29:06 +0200

    rgb version added

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 	const [nr, setNr] = useState(0);
 	const [showAnswer, setShowAnswer] = useState(false);
 	const [attempts, setAttempts] = useState(0);
+	const [colortype, setColortype] = useState('rgb');
 	const generateRandomNumber = () => {
 		let rnr = Math.floor(Math.random() * 256);
 		return rnr;
@@ -22,18 +23,45 @@ function App() {
 		setAttempts((a) => a + 1);
 		setShowAnswer(true);
 	};
-	useEffect(() => {
-		let newColorA = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
-		let newColorB = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
-		let newColorC = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
-		let newColorD = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
+	const restart = () => {
+		let hexA = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
+		let hexB = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
+		let hexC = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
+		let hexD = `#${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}${generateRandomNumber().toString(16)}`;
+		let rgbA = `rgb(${generateRandomNumber()},${generateRandomNumber()},${generateRandomNumber()})`;
+		let rgbB = `rgb(${generateRandomNumber()},${generateRandomNumber()},${generateRandomNumber()})`;
+		let rgbC = `rgb(${generateRandomNumber()},${generateRandomNumber()},${generateRandomNumber()})`;
+		let rgbD = `rgb(${generateRandomNumber()},${generateRandomNumber()},${generateRandomNumber()})`;
+
+		let newColorA = colortype === 'rgb' ? rgbA : hexA;
+		let newColorB = colortype === 'rgb' ? rgbB : hexB;
+		let newColorC = colortype === 'rgb' ? rgbC : hexC;
+		let newColorD = colortype === 'rgb' ? rgbD : hexD;
+
 		const colorsArr = [newColorA, newColorB, newColorC, newColorD];
 		setColorArr(colorsArr);
 		let rnrAD = Math.floor(Math.random() * 4);
 		setColor(colorsArr[rnrAD]);
-	}, [nr]);
+	};
+	const changeColorType = (ct: string) => {
+		setAttempts(0);
+		setNr(0);
+		setColortype(ct);
+	};
+	useEffect(() => {
+		restart();
+	}, [nr, colortype]);
 	return (
 		<div className={styles.container}>
+			<div className={styles.grid}>
+				<div onClick={() => changeColorType('rgb')} className={styles['grid-box']}>
+					RGB
+				</div>
+				<div onClick={() => changeColorType('hex')} className={styles['grid-box']}>
+					HEX
+				</div>
+			</div>
+
 			<h2>choose correct color </h2>
 			<div className={styles.box} style={{ background: color }}></div>
 			<div className={styles.grid}>
